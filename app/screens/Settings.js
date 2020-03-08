585a93d522a3f1052cd753607d65ed1271fbaf65
Tony Fung <tfung5@gmail.com>
2020-03-07 19:06:10 -0500

    Added 'Update Payment Method' button to Settings.

--- app/screens/Settings.js
+++ app/screens/Settings.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {
   Image,
+  Modal,
   StyleSheet,
   ScrollView,
   TextInput,
@@ -9,7 +10,7 @@ import {
 } from "react-native";
 import { NavigationActions } from "react-navigation";
 import Slider from "react-native-slider";
-
+import CreditCard from "./CreditCard";
 import { Divider, Button, Block, Text, Switch } from "../components";
 import { theme, mocks } from "../constants";
 
@@ -26,7 +27,8 @@ class Settings extends Component {
     notifications: true,
     newsletter: false,
     editing: null,
-    profile: {}
+    profile: {},
+    isModalVisible: false
   };
 
   budgetValues = [0.05, 0.1, 0.25, 0.5, 1.0];
@@ -46,6 +48,18 @@ class Settings extends Component {
     const { editing } = this.state;
     this.setState({ editing: !editing ? name : null });
   }
+
+  showModal = () => {
+    this.setState({
+      isModalVisible: true
+    });
+  };
+
+  hideModal = () => {
+    this.setState({
+      isModalVisible: false
+    });
+  };
 
   renderEdit(name) {
     const { profile, editing } = this.state;
@@ -84,118 +98,134 @@ class Settings extends Component {
     const { navigation } = this.props;
 
     return (
-      <Block>
-        <Block flex={false} row center space="between" style={styles.header}>
-          <Text h1 bold>
-            Settings
-          </Text>
-          <Button>
-            <Image source={profile.avatar} style={styles.avatar} />
-          </Button>
-        </Block>
-
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Block style={styles.inputs}>
-            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
-              <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>
-                  Username
-                </Text>
-                {this.renderEdit("username")}
-              </Block>
-              <Text
-                medium
-                secondary
-                onPress={() => this.toggleEdit("username")}
-              >
-                {editing === "username" ? "Save" : "Edit"}
-              </Text>
-            </Block>
-            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
-              <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>
-                  Location
-                </Text>
-                {this.renderEdit("location")}
-              </Block>
-              <Text
-                medium
-                secondary
-                onPress={() => this.toggleEdit("location")}
-              >
-                {editing === "location" ? "Save" : "Edit"}
-              </Text>
-            </Block>
-            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
-              <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>
-                  E-mail
-                </Text>
-                <Text bold>{profile.email}</Text>
-              </Block>
-            </Block>
-          </Block>
-
-          <Divider margin={[theme.sizes.base, theme.sizes.base * 2]} />
-
-          <Block style={styles.sliders}>
-            <Block margin={[10, 0]}>
-              <Text gray2 style={{ marginBottom: 10 }}>
-                Round Up
-              </Text>
-              {this.renderSlider(this.budgetValues, "budgetValuesIndex")}
-              <Text caption gray right>
-                {"Nearest "}
-                {this.budgetValues[this.state.budgetValuesIndex] >= 1
-                  ? "Dollar"
-                  : this.budgetValues[this.state.budgetValuesIndex] * 100 +
-                    " Cents"}
-              </Text>
-            </Block>
-            <Block margin={[10, 0]}>
-              <Text gray2 style={{ marginBottom: 10 }}>
-                Monthly Donation Cap
-              </Text>
-              <Slider
-                minimumValue={5}
-                maximumValue={100}
-                step={5}
-                style={{ height: 19 }}
-                thumbStyle={styles.thumb}
-                trackStyle={{ height: 6, borderRadius: 6 }}
-                minimumTrackTintColor={theme.colors.secondary}
-                maximumTrackTintColor="rgba(157, 163, 180, 0.10)"
-                value={this.state.monthly}
-                onValueChange={value => {
-                  this.setState({
-                    monthly: Math.round(value * 100) / 100
-                  });
-                }}
-              />
-              <Text caption gray right>
-                {this.state.monthly.toFixed(2)}
-              </Text>
-            </Block>
-          </Block>
-          <Divider />
-          <Block middle margin={[20]} padding={[theme.sizes.base / 2, 0]}>
-            <Button
-              gradient
-              onPress={() =>
-                navigation.navigate(
-                  "MainStack",
-                  {},
-                  NavigationActions.navigate({ routeName: "Welcome" })
-                )
-              }
-            >
-              <Text white center>
-                Sign Out
-              </Text>
+      <React.Fragment>
+        <Modal
+          animationType="slide"
+          visible={this.state.isModalVisible}
+          onRequestClose={this.hideModal}
+        >
+          <CreditCard navigation={navigation} />
+        </Modal>
+        <Block>
+          <Block flex={false} row center space="between" style={styles.header}>
+            <Text h1 bold>
+              Settings
+            </Text>
+            <Button>
+              <Image source={profile.avatar} style={styles.avatar} />
             </Button>
           </Block>
-        </ScrollView>
-      </Block>
+
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Block style={styles.inputs}>
+              <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+                <Block>
+                  <Text gray2 style={{ marginBottom: 10 }}>
+                    Username
+                  </Text>
+                  {this.renderEdit("username")}
+                </Block>
+                <Text
+                  medium
+                  secondary
+                  onPress={() => this.toggleEdit("username")}
+                >
+                  {editing === "username" ? "Save" : "Edit"}
+                </Text>
+              </Block>
+              <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+                <Block>
+                  <Text gray2 style={{ marginBottom: 10 }}>
+                    Location
+                  </Text>
+                  {this.renderEdit("location")}
+                </Block>
+                <Text
+                  medium
+                  secondary
+                  onPress={() => this.toggleEdit("location")}
+                >
+                  {editing === "location" ? "Save" : "Edit"}
+                </Text>
+              </Block>
+              <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+                <Block>
+                  <Text gray2 style={{ marginBottom: 10 }}>
+                    E-mail
+                  </Text>
+                  <Text bold>{profile.email}</Text>
+                </Block>
+              </Block>
+            </Block>
+
+            <Divider margin={[theme.sizes.base, theme.sizes.base * 2]} />
+
+            <Block style={styles.sliders}>
+              <Block margin={[10, 0]}>
+                <Text gray2 style={{ marginBottom: 10 }}>
+                  Round Up
+                </Text>
+                {this.renderSlider(this.budgetValues, "budgetValuesIndex")}
+                <Text caption gray right>
+                  {"Nearest "}
+                  {this.budgetValues[this.state.budgetValuesIndex] >= 1
+                    ? "Dollar"
+                    : this.budgetValues[this.state.budgetValuesIndex] * 100 +
+                      " Cents"}
+                </Text>
+              </Block>
+              <Block margin={[10, 0]}>
+                <Text gray2 style={{ marginBottom: 10 }}>
+                  Monthly Donation Cap
+                </Text>
+                <Slider
+                  minimumValue={5}
+                  maximumValue={100}
+                  step={5}
+                  style={{ height: 19 }}
+                  thumbStyle={styles.thumb}
+                  trackStyle={{ height: 6, borderRadius: 6 }}
+                  minimumTrackTintColor={theme.colors.secondary}
+                  maximumTrackTintColor="rgba(157, 163, 180, 0.10)"
+                  value={this.state.monthly}
+                  onValueChange={value => {
+                    this.setState({
+                      monthly: Math.round(value * 100) / 100
+                    });
+                  }}
+                />
+                <Text caption gray right>
+                  {this.state.monthly.toFixed(2)}
+                </Text>
+              </Block>
+            </Block>
+            <Divider />
+            <Block middle margin={[10]} padding={0}>
+              <Button gradient onPress={this.showModal}>
+                <Text white center>
+                  Update Payment Method
+                </Text>
+              </Button>
+            </Block>
+            <Block middle margin={[10]} padding={0}>
+              <Button
+                gradient
+                onPress={() =>
+                  navigation.navigate(
+                    "MainStack",
+                    {},
+                    NavigationActions.navigate({ routeName: "Welcome" })
+                  )
+                }
+              >
+                <Text white center>
+                  Sign Out
+                </Text>
+              </Button>
+            </Block>
+          </ScrollView>
+        </Block>
+      </React.Fragment>
     );
   }
 }
